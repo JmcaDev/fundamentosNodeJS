0cd56b50e1689cb9102aef40b07109836fdf8c63
JmcaDev <jmcavzla@gmail.com>
2025-05-23 19:37:38 -0400

    Destructuring and default env variables

---
+++ 02-Bases/src/js-foundations/02-destructuring.js
@@ -0,0 +1,13 @@
+
+
+// console.log(process.env)
+
+const { PROMPT, windir } = process.env
+
+// console.table({PROMPT, windir})
+
+const characteres = ["Flash", "Superman", "Batman"]
+
+const [flash, superman, batman] = characteres
+
+// console.log(batman)
