60c325d333939ed22f068dad8b65b7fc746bf6b3
JmcaDev <jmcavzla@gmail.com>
2025-05-23 20:03:04 -0400

    Callbacks

--- 02-Bases/src/app.js
+++ 02-Bases/src/app.js
@@ -1,5 +1,17 @@
 
-const {emailTemplate} = require("./js-foundations/01-template")
-require("./js-foundations/02-destructuring")
-
+// const {emailTemplate} = require("./js-foundations/01-template")
 // console.log(emailTemplate)
+
+
+// require("./js-foundations/02-destructuring")
+const {getUserById} = require("./js-foundations/03-callbacks")
+
+const id = 3
+
+getUserById(id, function(error, user){
+    if(error){
+        throw new Error(error)
+    }
+
+    console.log(user)
+})
